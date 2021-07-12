77a7100192ec71f4ad5448abb4de94eeb5c2c1ec
Mark <mark@example.dev>
2021-07-11 22:15:05 -0700

    further study

--- movie-app.js
+++ movie-app.js
@@ -1,4 +1,8 @@
-$('form').submit(function(e){
+let alphaSortDir = true; //false: z to a, true: a to z;
+let numSortDir = false; //false: highest to lowest, true: lowest to highest
+
+
+$('#movie-form').submit(function(e){
     e.preventDefault();
 
     const title = $('input[type="text"]').val();
@@ -8,6 +12,7 @@ $('form').submit(function(e){
     const $rating = $(`<p>${rating}/10</p>`);
     $('<div>')
         .addClass('movie-block')
+        .attr({'data-rating': rating, 'data-title': title})
         .append($title)
         .append($rating)
         .append('<button>X</button>')
@@ -17,3 +22,51 @@ $('form').submit(function(e){
 $('#movie-container').on('click', 'button', function(){
     $(this).parent().remove();
 })
+
+$('#sort-form').submit(function(e){
+    e.preventDefault();
+
+    //get all movie-block divs
+    const $movies = $('.movie-block');
+    //title sort, alphabetical
+    if(this.sortType[0].checked){
+        sortDiv('', $movies, alphaSortDir);
+        $movies.appendTo('#movie-container');
+        alphaSortDir = !alphaSortDir;
+        numSortDir = false;
+    }
+    //rating sort
+    else if(this.sortType[1].checked){
+        sortDiv(0, $movies, numSortDir);
+        $movies.appendTo('#movie-container');
+        numSortDir = !numSortDir;
+        alphaSortDir = true;
+    }
+    else{
+        return;
+    }
+})
+
+// Mode - pass in a string to sort alphabetically, pass in a number to sort numerically
+// Direction - boolean, false sorts Highest to Lowest, true sorts Lowest to Highest
+function sortDiv(mode, jqObj, direction){
+    let noSortsThisPass = false;
+    while (!noSortsThisPass){
+        noSortsThisPass = true;
+        for (let i = 0; i < jqObj.length - 1; i++){
+            let i0, i1;
+            if (typeof mode === 'number'){
+                i0 = +$(jqObj[i]).attr('data-rating');
+                i1 = +$(jqObj[i+1]).attr('data-rating');
+            } else if (typeof mode === 'string'){
+                i0 = $(jqObj[i]).attr('data-title');
+                i1 = $(jqObj[i+1]).attr('data-title');
+            }
+            const sort = direction ? (i0 > i1) : (i0 < i1);
+            if (sort){
+                [jqObj[i], jqObj[i+1]] = [jqObj[i+1], jqObj[i]];
+                noSortsThisPass = false;
+            }
+        }
+    }
+}
